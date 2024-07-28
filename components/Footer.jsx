116cb810de84b2bb0c3ec2dee7b150db82b9617a
soodaayush <aayush.sood@icloud.com>
2024-07-28 13:38:54 -0300

    Reorganized footer file

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,13 @@
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Linking from "expo-linking";
+import { router } from "expo-router";
 
 import Colors from "../constants/colors";
 
 import Map from "../assets/footer-icons/map.svg";
 import Website from "../assets/footer-icons/website.svg";
 import Phone from "../assets/footer-icons/phone.svg";
-import { router } from "expo-router";
 
 const Footer = () => {
   async function openURL(url) {
